09ef379704b8e99d07b9e3493d4e9b5951f4844b
medhaouari7 <medhaouari025@gmail.com>
2022-03-14 16:53:23 +0100

    Responsive

--- travel-website/src/components/navbar/Navbar.js
+++ travel-website/src/components/navbar/Navbar.js
@@ -5,6 +5,8 @@ import { HiOutlineMenuAlt4 } from 'react-icons/hi'
 import { AiOutlineClose } from 'react-icons/ai'
 import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa'
 
+import { Link } from 'react-scroll'
+
 import './NavbarStyle.css'
 
 function NavBar() {
@@ -12,16 +14,16 @@ function NavBar() {
   const handleNav = () => setNav (!nav)
 
   return (
-    <div className={ nav ? 'navbar navbar-bg' : 'navbar'}>
+    <div name='home' className={ nav ? 'navbar navbar-bg' : 'navbar'}>
         <div className={nav ? 'logo dark' : 'logo'}>
          <h2>BEACHES.</h2>
         </div>
         <ul className="nav-menu">
-          <li>Home</li>
-          <li>Destinations</li>
-          <li>Travel</li>
-          <li>Book</li>
-          <li>Views</li>
+        <Link to='home' smooth={true} duration={500} ><li>Home</li></Link>
+                <Link to='destinations' smooth={true} duration={500} ><li>Destinations</li></Link>
+                <Link to='carousel' smooth={true} duration={500} ><li>Travel</li></Link>
+                <Link to='search' smooth={true} duration={500} ><li>Book</li></Link>
+                <Link to='views' smooth={true} duration={500} ><li>Views</li></Link>
         </ul>
       <div className="nav-icons">
        <BiSearch className="icon" style={{marginRight: '1rem' }}/>
@@ -32,11 +34,11 @@ function NavBar() {
       </div>
       <div className={ nav ? 'mobile-menu active' : 'mobile-menu'}>
          <ul className="mobile-nav">
-        <li>Home</li>
-        <li>Destinations</li>
-        <li>Travel</li>
-        <li>Book</li>
-        <li>Views</li>
+         <Link to='home' smooth={true} duration={500} ><li>Home</li></Link>
+                <Link to='destinations' smooth={true} duration={500} ><li>Destinations</li></Link>
+                <Link to='carousel' smooth={true} duration={500} ><li>Travel</li></Link>
+                <Link to='search' smooth={true} duration={500} ><li>Book</li></Link>
+                <Link to='views' smooth={true} duration={500} ><li>Views</li></Link>
              </ul> 
              <div className="mobile-menu-bottom">
                  <div className="menu-icons">
