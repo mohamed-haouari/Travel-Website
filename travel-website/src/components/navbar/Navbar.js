8285dbceff213f311d3c7039c9235bf76c8cf6a1
medhaouari7 <medhaouari025@gmail.com>
2022-03-14 14:52:51 +0100

    Hero

--- travel-website/src/components/navbar/Navbar.js
+++ travel-website/src/components/navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { BiSearch } from 'react-icons/bi'
 import { BsPerson } from 'react-icons/bs'
 import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import { AiOutlineClose } from 'react-icons/ai'
 import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 import './NavbarStyle.css'
@@ -12,7 +13,7 @@ function NavBar() {
 
   return (
     <div className={ nav ? 'navbar navbar-bg' : 'navbar'}>
-        <div className="logo">
+        <div className={nav ? 'logo dark' : 'logo'}>
          <h2>BEACHES.</h2>
         </div>
         <ul className="nav-menu">
@@ -27,7 +28,7 @@ function NavBar() {
        <BsPerson className="icon" />
       </div>
       <div className="hamburger" onClick={handleNav}>
-           <HiOutlineMenuAlt4 className="icon"/>
+      {!nav ? (<HiOutlineMenuAlt4 className='icon' />) : (<AiOutlineClose style={{ color: '#000' }} className='icon' />)}
       </div>
       <div className={ nav ? 'mobile-menu active' : 'mobile-menu'}>
          <ul className="mobile-nav">
