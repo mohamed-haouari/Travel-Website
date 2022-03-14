18d42aee6213a20d3c600445abdb5a3d265b99e5
medhaouari7 <medhaouari025@gmail.com>
2022-03-14 12:01:49 +0100

    navbar-2

--- travel-website/src/components/navbar/Navbar.js
+++ travel-website/src/components/navbar/Navbar.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { BiSearch } from 'react-icons/bi'
 import { BsPerson } from 'react-icons/bs'
+import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa'
+
+import './NavbarStyle.css'
 
 function NavBar() {
   return (
@@ -18,6 +22,31 @@ function NavBar() {
       <div className="nav-icons">
        <BiSearch className="icon" />
        <BsPerson className="icon" />
+      </div>
+      <div className="hamburger">
+           <HiOutlineMenuAlt4 className="icon"/>
+      </div>
+      <div className="mobile-menu">
+         <ul className="mobile-nav">
+         <li>Home</li>
+          <li>Destinations</li>
+          <li>Travel</li>
+          <li>Book</li>
+          <li>Views</li>
+             </ul> 
+             <div className="mobile-menu-bottom">
+                 <div className="menu-icons">
+                <button>Search</button>
+                <button>Account</button>
+                 </div>
+                 <div className="social-icons">
+                        <FaFacebook className='icon' />
+                        <FaInstagram className='icon' />
+                        <FaTwitter className='icon' />
+                        <FaPinterest className='icon' />
+                        <FaYoutube className='icon' />
+                 </div>
+             </div>
       </div>
     </div>
   )
